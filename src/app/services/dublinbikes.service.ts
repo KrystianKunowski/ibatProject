144a88a98ab02726ef7f067d213cae29a357ff99
KrystianKunowski <krystian.kunowski.home@gmail.com>
2020-08-26 21:13:10 +0100

    1st commit

--- src/app/services/dublinbikes.service.ts
+++ src/app/services/dublinbikes.service.ts
@@ -6,15 +6,15 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class DublinbikesService {
-  
+
   private contract_name = `dublin`;
   private api_key = `923b9f8762f3bacf246b898bf50e66550436b145`;
-  private url = `https://api.jcdecaux.com/vls/v1/stations?contract=${this.contract_name}&apiKey=${this.api_key}`;
+  private Bikeurl = `https://api.jcdecaux.com/vls/v1/stations?contract=${this.contract_name}&apiKey=${this.api_key}`;
 
 
   constructor(private http: HttpClient) { }
 
   getListOfBikes(): Observable<Dublinbikes[]>{
-    return this.http.get<Dublinbikes[]>(this.url);
+    return this.http.get<Dublinbikes[]>(this.Bikeurl);
   }
 }
